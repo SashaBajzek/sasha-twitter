7043b2abbf6385ae2610b0ff4f3a65b5b1b2e406
Sasha Bajzek <dabridginator@gmail.com>
2018-09-11 16:10:20 -0700

    Add tweet length validation

--- src/components/SubmitTweetForm/SubmitTweetForm.tsx
+++ src/components/SubmitTweetForm/SubmitTweetForm.tsx
@@ -4,14 +4,24 @@ import { Field, InjectedFormProps, reduxForm } from "redux-form";
 import CharacterCounter from "../CharacterCounter/CharacterCounter";
 
 interface ICustomProps {
-  validated: boolean;
+  validated: boolean; // Delete this
 }
+
+const validate = (values: any) => {
+  const errors: any = {};
+  if (!values.tweetText) {
+    errors.tweetText = "tweet required";
+  } else if (values.tweetText.length > 140) {
+    errors.tweetText = "Must be less than or equal to 140 characters";
+  }
+  return errors;
+};
 
 class FormComponent extends React.Component<
   ICustomProps & InjectedFormProps<{}, ICustomProps>
 > {
   public render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit, valid } = this.props;
 
     return (
       <form onSubmit={handleSubmit}>
@@ -20,14 +30,17 @@ class FormComponent extends React.Component<
           <Field name="tweetText" component="textarea" type="text" />
         </div>
         <CharacterCounter />
-        <button type="submit">Submit Tweet</button>
+        <button type="submit" disabled={!valid}>
+          Submit Tweet
+        </button>
       </form>
     );
   }
 }
 
 const SubmitTweetForm = reduxForm<{}, ICustomProps>({
-  form: "createTweetForm" // a unique name for the form
+  form: "createTweetForm", // a unique name for the form
+  validate
 })(FormComponent);
 
 export default SubmitTweetForm;
